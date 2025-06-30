cd3245081cee6538b1b799ff8590f4cb5b757006
David Bower <dabower787@gmail.com>
2025-06-29 23:13:06 -0400

    Started w/ python script

---
+++ Scripts/httpUp.py
@@ -0,0 +1,23 @@
+'''
+    Name: Python listener
+
+    Desc:
+        To run on target system 
+'''
+
+import socket
+import subprocess
+import os
+
+# Global Host var
+# Global prt var
+HOST = "0.0.0.0"
+PORT = 9999
+
+
+
+def main():
+    print("HTTP Server is up!") 
+
+if __name__ == "__main__":
+    main()
